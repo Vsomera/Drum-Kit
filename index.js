fe306ecabd244cb89bae0893d9b8ea33951c64a2
Vilmor Somera <91754316+Vsomera@users.noreply.github.com>
2023-01-05 19:30:05 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+var drumButtons = document.getElementsByClassName("drum")
+
+for (let x = 0; x < drumButtons.length; x ++) {
+    drumButtons[x].addEventListener("click", function() {      // listens for button clicks on all buttons
+        playAudio(this.innerHTML)                              // sends key input to playAudio function
+    });
+}
+
+document.addEventListener("keydown", function(event) {              // listens for keyboard input on website
+    playAudio((event.key).toLowerCase())                            // sends key input to playAudio function
+})
+
+function playAudio(keyVal) { 
+    /*
+        Function plays audio according to keys pressed or clicked by the user
+    */                               
+    switch(keyVal) {                                
+        case 'w':
+            var audio = new Audio("/sounds/crash.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio("/sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("/sounds/snare.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("/sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("/sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("/sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("/sounds/tom-4.mp3");
+            audio.play();
+            break;
+    }   
+}
+
